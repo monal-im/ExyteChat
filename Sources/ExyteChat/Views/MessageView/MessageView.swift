4f51a6c535470f60814a08d3096ee75e226ffdfc
lissine <lissine@ellini.org>
2025-08-28 17:15:52 +0100

    MessageView: make the message property an ObservedObject
    This is needed to get automatic view re-renders when the
    ChatViewMessage changes (i.e. when the underlying MLMessage changes)

--- Sources/ExyteChat/Views/MessageView/MessageView.swift
+++ Sources/ExyteChat/Views/MessageView/MessageView.swift
@@ -16,7 +16,7 @@ struct MessageView: View {
 
     @ObservedObject var viewModel: ChatViewModel
 
-    let message: Message
+    @ObservedObject var message: Message
     let positionInUserGroup: PositionInUserGroup
     let positionInMessagesSection: PositionInMessagesSection
     let chatType: ChatType

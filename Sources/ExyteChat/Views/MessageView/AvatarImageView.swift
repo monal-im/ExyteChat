6e48198ece2123b79d07697ce9d8e5549fc8178f
lissine <lissine@ellini.org>
2025-09-30 05:54:03 +0100

    Add support for passing avatars as UIImage
    Co-authored-by: Thilo Molitor <thilo@eightysoft.de>

--- Sources/ExyteChat/Views/MessageView/AvatarImageView.swift
+++ Sources/ExyteChat/Views/MessageView/AvatarImageView.swift
@@ -11,15 +11,23 @@ struct AvatarImageView: View {
     var avatarCacheKey: String? = nil
 
     var body: some View {
-        CachedAsyncImage(url: user?.avatarURL, cacheKey: avatarCacheKey) { image in
-            image
+        if let user = user, let avatarImage = user.avatarImage {
+            Image(uiImage: avatarImage)
                 .resizable()
                 .scaledToFill()
-        } placeholder: {
-            Rectangle().fill(Color.gray)
+                .viewSize(avatarSize)
+                .clipShape(Circle())
+        } else {
+            CachedAsyncImage(url: user?.avatarURL, cacheKey: avatarCacheKey) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                Rectangle().fill(Color.gray)
+            }
+            .viewSize(avatarSize)
+            .clipShape(Circle())
         }
-        .viewSize(avatarSize)
-        .clipShape(Circle())
     }
 }
 

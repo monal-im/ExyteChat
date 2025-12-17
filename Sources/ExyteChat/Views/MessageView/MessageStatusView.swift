ed600e2afe39b88ea41aa13cc65b49b8a1611b3f
lissine <lissine@ellini.org>
2025-12-17 23:05:40 +0100

    Revert "Added delivered case to Message.Status" and its fixup commit
    This reverts commits 8e9b0615d96909ff1d53cb07b73d6ddb8fec26dd
    and baaea88c620c0850cde22f724a97dd1164f739d9.

--- Sources/ExyteChat/Views/MessageView/MessageStatusView.swift
+++ Sources/ExyteChat/Views/MessageView/MessageStatusView.swift
@@ -15,52 +15,36 @@ struct MessageStatusView: View {
         Group {
             switch status {
             case .sending:
-                statusImageStyled(image: theme.images.message.sending, color: theme.colors.statusGray)
+                theme.images.message.sending
+                    .resizable()
+                    .foregroundColor(theme.colors.statusGray)
             case .sent:
-                statusImageStyled(image: theme.images.message.sent, color: theme.colors.statusGray)
-            case .delivered:
-                statusImageStyled(image: theme.images.message.delivered, color: theme.colors.statusGray)
+                theme.images.message.sent
+                    .resizable()
+                    .foregroundColor(theme.colors.messageMyBG)
             case .read:
-                statusImageStyled(image: theme.images.message.read, color: theme.colors.messageReadStatus)
+                theme.images.message.read
+                    .resizable()
+                    .foregroundColor(theme.colors.messageMyBG)
             case .error:
-                Button(action: onRetry) {
-                    statusImageStyled(image: theme.images.message.error, color: theme.colors.statusError)
+                Button {
+                    onRetry()
+                } label: {
+                    theme.images.message.error
+                        .resizable()
                 }
+                .foregroundColor(theme.colors.statusError)
             }
         }
-    }
-
-    private func statusImageStyled(image: Image, color: Color) -> some View {
-        image
-            .renderingMode(.template)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .foregroundColor(color)
-            .frame(width: 40)
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
-    
     static var previews: some View {
         VStack {
             MessageStatusView(status: .sending, onRetry: {})
             MessageStatusView(status: .sent, onRetry: {})
-            MessageStatusView(status: .delivered, onRetry: {})
             MessageStatusView(status: .read, onRetry: {})
-            MessageStatusView(status: .error(emptyDraft()), onRetry: {})
         }
-    }
-
-    private static func emptyDraft() -> DraftMessage {
-        return DraftMessage(
-            id: nil,
-            text: "",
-            medias: [],
-            recording: nil,
-            replyMessage: nil,
-            createdAt: Date()
-        )
-
     }
 }

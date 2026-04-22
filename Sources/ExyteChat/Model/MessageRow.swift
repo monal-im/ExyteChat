ca0ab24821b98cfe092502bc2cedd7f44aec8692
lissine <lissine@ellini.org>
2026-04-22 20:17:39 +0100

    Don't compare the entire message objects to determine MessageRow equality
    This is a performance optimization. It should make loading the ChatView
    and scrolling it a bit faster.
    
    Note: this might cause bugs if / when upload status displaying is
    implemented in Monal. Revert this commit if that's the case (or add the
    relevant property to the MessageRow equality check)
    
    This commit is a partial revert of commit cff61f6f66182ee0

--- Sources/ExyteChat/Model/MessageRow.swift
+++ Sources/ExyteChat/Model/MessageRow.swift
@@ -80,7 +80,10 @@ struct MessageRow: Equatable {
         && lhs.positionInUserGroup == rhs.positionInUserGroup
         && lhs.positionInMessagesSection == rhs.positionInMessagesSection
         && lhs.commentsPosition == rhs.commentsPosition
-        && lhs.message == rhs.message
+        && lhs.message.status == rhs.message.status
+        && lhs.message.triggerRedraw == rhs.message.triggerRedraw
+        && lhs.message.attributedText == rhs.message.attributedText
+        && lhs.message.reactions == rhs.message.reactions
     }
 }
 

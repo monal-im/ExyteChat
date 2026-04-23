dad115ac46ac36b2efa45cbdb9960038e6f8ded9
lissine <lissine@ellini.org>
2026-04-23 10:43:47 +0100

    Focus the input TextView on avatar tap

--- Sources/ExyteChat/Views/MessageView/MessageView.swift
+++ Sources/ExyteChat/Views/MessageView/MessageView.swift
@@ -231,12 +231,14 @@ struct MessageView: View {
                         .contentShape(Circle())
                         .onTapGesture {
                             params.tapAvatarClosure?(message.user, message.id)
+                            viewModel.focusTheInputTextView()
                         }
                 } else {
                     AvatarNameView(name: message.user.name, avatarSize: params.avatarSize)
                         .contentShape(Circle())
                         .onTapGesture {
                             params.tapAvatarClosure?(message.user, message.id)
+                            viewModel.focusTheInputTextView()
                         }
                 }
             } else {

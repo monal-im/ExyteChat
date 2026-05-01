32ff51722c98dfd297f9320cdf12840d6118d163
lissine <lissine@ellini.org>
2026-05-01 10:42:32 +0100

    Increase the maximum width of the reaction selection view on macOS
    Some macOS users don't have a trackpad / Apple mouse, so they can't
    scroll the reaction selection view.
    Increase the width so all the emojis are visible, including the
    "button" that lets you select any emoji.

--- Sources/ExyteChat/Views/MessageView/MessageMenu/MessageMenu+ReactionSelectionView.swift
+++ Sources/ExyteChat/Views/MessageView/MessageMenu/MessageMenu+ReactionSelectionView.swift
@@ -9,7 +9,14 @@ struct ReactionSelectionView: View {
     
     @Environment(\.dynamicTypeSize) private var dynamicTypeSize
     
+#if targetEnvironment(macCatalyst)
+    // Some macOS users don't have a trackpad / Apple mouse,
+    // so they can't scroll the reaction selection view.
+    // Increase the max width so all the emojis are visible.
+    static let maxSelectionRowWidth: CGFloat = 800
+#else
     static let maxSelectionRowWidth: CGFloat = 400
+#endif
 
     @StateObject private var keyboardState = KeyboardState()
 

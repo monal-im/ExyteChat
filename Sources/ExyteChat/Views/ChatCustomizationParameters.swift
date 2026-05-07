abf1c3a09cb876d9ed20238bd7eb0e81d8933fdf
lissine <lissine@ellini.org>
2026-05-08 00:58:42 +0100

    Remove media from the available inputs on macOS
    The media picker isn't that useful there.
    Users can upload media via the file picker.

--- Sources/ExyteChat/Views/ChatCustomizationParameters.swift
+++ Sources/ExyteChat/Views/ChatCustomizationParameters.swift
@@ -75,7 +75,11 @@ struct MessageCustomizationParameters {
 struct InputViewCustomizationParameters {
     var externalInputText: String? // External → Internal
     var onInputTextChange: ((String) -> Void)? // Internal → External
+#if targetEnvironment(macCatalyst)
+    var availableInputs: [AvailableInputType] = [.text, .audio, .file]
+#else
     var availableInputs: [AvailableInputType] = [.text, .audio, .media, .file]
+#endif
     var recorderSettings = RecorderSettings()
     var audioRecordingMode: AudioRecordingMode = .holdToRecord
     var mediaPickerParameters = MediaPickerParameters()
